afe8a058dde4cf423420608604b2700d87e11f73
bxnjjaaa23 <168591969+bxnjjaaa23@users.noreply.github.com>
2024-07-02 16:42:09 -0400

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,14 @@
+from random import *
+folio=0
+def primer_Folio():
+    folio=randint(403, 15489)
+    print(folio)
+    return(folio)
+
+folio_resultado=primer_Folio()
+folio_resultado+=1
+print(folio_resultado)
+folio_resultado+=1
+print(folio_resultado)
+folio_resultado+=1
+print(folio_resultado)
